fb1964b0e58341a5cd241843921c1a6bb9c3c045
Anjali Goel <anjaligoel@anjalis-mbp.mynetworksettings.com>
2024-07-05 10:35:51 -0400

    checkpoint 7/5

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='montecarlo',
+    version='1.0.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A package for Monte Carlo simulation',
+    packages=find_packages(),
+    install_requires=[
+        'pandas'
+    ],
+)
