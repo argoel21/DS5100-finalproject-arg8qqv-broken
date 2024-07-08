915ff96140f0190956d4a974edbe99583cc11266
Anjali Goel <anjaligoel@Anjalis-MacBook-Pro.local>
2024-07-08 08:56:56 -0400

    7/8

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,6 +1,20 @@
+import numpy as np
+import pandas as pd
+
 class Die():
-    def __init__(self):
-        pass
+    def __init__(self, faces):
+        if not isinstance(faces, np.ndarray):
+            raise TypeError("Input faces should be a numpy array")
+        weights = np.ones_like(faces, dtype=float)
+        self.die_data = pd.DataFrame({'weights': weights}, index=faces)
+    
+    def play(self, rolls):
+        results = self.die_data.sample(n=times, weights='weights', replace=True).index.tolist()
+        return results
+    
+    def results(self, form = 'wide'):
+        return self.die_data.copy()
+
 
 class Game():
     def __init__(self):
